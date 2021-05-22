1e71911b3078f649172d3ccba5c3929c1850d548
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2021-05-22 13:49:20 +0530

    setup file

---
+++ setupFile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+git clone  https://github.com/dineshcode97/Docker-Setup-Using-Bash-Scripting.git
+chmod +x /Docker-Setup-Using-Bash-Scripting/configureDocker.sh
+./configureDocker.sh
